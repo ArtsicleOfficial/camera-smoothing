b3e42facd2940519e4d4a85408dbebdb86884df4
ArtsicleOfficial <adegqhou>
2021-10-18 18:05:14 -0400

    Added % unit

--- src/main/java/com/camerasmoothing/CameraSmoothingConfig.java
+++ src/main/java/com/camerasmoothing/CameraSmoothingConfig.java
@@ -5,14 +5,16 @@ import net.runelite.client.config.*;
 @ConfigGroup("camerasmoothing")
 public interface CameraSmoothingConfig extends Config
 {
-
+	@Units(
+			"%"
+	)
 	@Range(
 			min = 0,
 			max = 100
 	)
 	@ConfigItem(
 		keyName = "smoothingSpeed",
-		name = "Smoothness (0-100%)",
+		name = "Smoothness",
 		description = "How slowly the camera moves (0-100%)"
 	)
 	default int smoothness()
