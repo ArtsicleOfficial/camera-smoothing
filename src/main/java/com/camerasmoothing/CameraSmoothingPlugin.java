e7e0b51605d8179869dcef2ecaab23c9b3db425e
= <=>
2021-10-19 11:48:51 -0400

    Added zoom smoothing and it looks incredible

--- src/main/java/com/camerasmoothing/CameraSmoothingPlugin.java
+++ src/main/java/com/camerasmoothing/CameraSmoothingPlugin.java
@@ -3,13 +3,11 @@ package com.camerasmoothing;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.VarClientInt;
+import net.runelite.api.*;
 import net.runelite.api.events.BeforeRender;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ScriptCallbackEvent;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -27,20 +25,61 @@ public class CameraSmoothingPlugin extends Plugin
 	@Inject
 	private CameraSmoothingConfig config;
 
+	@Inject
+	private ClientThread clientThread;
+
 	private final int HALF_ROTATION = 1024;
 	private final int FULL_ROTATION = 2048;
 
 	private final int PITCH_INDEX = 0;
 	private final int YAW_INDEX = 1;
+	private final int SCROLL_INDEX = 2;
 	//Made an array just in case a setter for camera pitch is ever added
-	private int[] deltaCamera = new int[2];
-	private int[] previousCamera = new int[2];
+	private int[] deltaCamera = new int[3];
+	private int[] previousCamera = new int[3];
 
 	private int lerp(int x, int y, float alpha) {
 		return x+(int)((y-x)*alpha);
 	}
 	private int getSmallestAngle(int x, int y) {
 		return ((y-x) + HALF_ROTATION) % FULL_ROTATION - HALF_ROTATION;
+	}
+	private void applySmoothingToAngle(int index) {
+		int deltaChange;
+		int changed;
+		int newDeltaAngle;
+		newDeltaAngle = getSmallestAngle(previousCamera[index],index == YAW_INDEX ? client.getMapAngle() : 0/*No pitch method in RL*/);
+		deltaCamera[index] += newDeltaAngle;
+
+		deltaChange = lerp(deltaCamera[index],0,(config.smoothness()/100.0f));
+		changed = previousCamera[index] + deltaChange;
+
+		deltaCamera[index] -= deltaChange;
+		if(index == YAW_INDEX) {
+			client.setCameraYawTarget(changed);
+		}/* else if(index == PITCH_INDEX) {
+			//No pitch method in RL yet
+		}*/
+		previousCamera[index] += deltaChange;
+	}
+	private void setZoom(int amount) {
+		clientThread.invoke(()-> client.runScript(ScriptID.CAMERA_DO_ZOOM,amount,amount));
+	}
+	private void applySmoothingToZoom(int index) {
+		int deltaChange;
+		int changed;
+		int newZoom;
+		newZoom = client.getVar(VarClientInt.CAMERA_ZOOM_RESIZABLE_VIEWPORT) - previousCamera[index];
+		deltaCamera[index] += newZoom;
+
+		deltaChange = lerp(deltaCamera[index],0,(config.smoothness()) / 100.0f);
+		changed = previousCamera[index] + deltaChange;
+
+		deltaCamera[index] -= deltaChange;
+		if(index == SCROLL_INDEX) {
+			setZoom(changed);
+		}
+		previousCamera[index] += deltaChange;
 	}
 
 	@Subscribe
@@ -54,17 +93,12 @@ public class CameraSmoothingPlugin extends Plugin
 
 		//Pitch stuff to be added if runelite ever decides to add a Client.setCameraPitchTarget method
 		//Until then, yaw going to have to stick with yaw!
+		//applySmoothingToAngle(PITCH_INDEX);
+		applySmoothingToAngle(YAW_INDEX);
 
-		newDeltaAngle = getSmallestAngle(previousCamera[YAW_INDEX],client.getMapAngle());
-		deltaCamera[YAW_INDEX] += newDeltaAngle;
+		applySmoothingToZoom(SCROLL_INDEX);
 
-		deltaChange = lerp(deltaCamera[YAW_INDEX],0,(config.smoothness()/100.0f));
-		changed = (previousCamera[YAW_INDEX] + deltaChange);
 
-		deltaCamera[YAW_INDEX] -= deltaChange;
-		client.setCameraYawTarget(changed);
-
-		previousCamera[YAW_INDEX] += deltaChange;
 	}
 
 	@Provides
